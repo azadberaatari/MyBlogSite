e804722f1179663cc48554a2ecd3c5bd0e5bcafc
azady <azady@SYSTEMOFADOWN>
2025-04-25 03:09:03 +0300

    writer panel güncellemeleri

--- MyBlogSite/Controllers/WriterController.cs
+++ MyBlogSite/Controllers/WriterController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyBlogSite.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyBlogSite.Controllers
 {
     public class WriterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -32,6 +39,47 @@ namespace MyBlogSite.Controllers
         {
             return PartialView();
         }
-
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writervalues = wm.TGetById(1);
+            return View(writervalues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p )
+        {
+            wm.TUpdate(p);
+            return RedirectToAction("Index" , "Dashboard");
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p )
+        {
+            Writer w = new Writer();
+            if (p.WriterImage!= null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterName = p.WriterName;
+            w.WriterAbout = p.WriterAbout;
+            w.WriterMail = p.WriterMail;
+            w.WriterStatus = true;
+            w.WriterPassword = p.WriterPassword;
+            wm.TAdd(w);
+            return RedirectToAction("Index", "Dashboard");
+        }
     }
 }
