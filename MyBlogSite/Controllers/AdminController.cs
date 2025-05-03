aa7e9d821efcaaeea7015f7a0397debc9b9d53bd
azady <azady@SYSTEMOFADOWN>
2025-05-03 15:51:16 +0300

    Admin Theme

---
+++ MyBlogSite/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
