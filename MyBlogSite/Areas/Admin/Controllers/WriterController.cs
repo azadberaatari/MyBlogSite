b6edbf7ad887177c088c214a83544af24a7312bb
azady <azady@SYSTEMOFADOWN>
2025-05-10 20:47:52 +0300

    yazar AJAX İŞLEMLERİ

---
+++ MyBlogSite/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyBlogSite.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace MyBlogSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findwriter = writers.FirstOrDefault(x => x.ID == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findwriter);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass writerClass)
+        {
+            writers.Add(writerClass);
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.ID == id);
+            writers.Remove(writer);
+
+            return Json(writer); //?
+        }
+
+        public IActionResult UpdateWriter(WriterClass writerClass)
+        {
+            var writer = writers.FirstOrDefault(x => x.ID == writerClass.ID);
+            writer.Name = writerClass.Name;
+            var jsonWriter = JsonConvert.SerializeObject(writerClass);
+            return Json(jsonWriter);
+
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                ID = 1,
+                Name="Efrun Evdi"
+            },
+            new WriterClass
+            {
+                ID = 2,
+                Name="Ali Evdi"
+            },
+            new WriterClass
+            {
+                ID = 3,
+                Name="Ömür Kırık"
+            },
+        };
+    }
+}
--- MyBlogSite/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ MyBlogSite/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace MyBlogSite.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,11 @@ namespace MyBlogSite.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            String api = "d932a968b28571e6df470c25af4225f8";
+            String connection = "https://api.openweathermap.org/data/2.5/weather?q=%C4%B0stanbul&mode=xml&lang=tr&units=metric&appid=" + api ;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
