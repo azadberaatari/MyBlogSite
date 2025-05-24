17dff07ee4780177d24480eabbc3ccb1e8b88d63
azady <azady@SYSTEMOFADOWN>
2025-05-24 03:34:33 +0300

    Rol İşlemleri

---
+++ MyBlogSite/Models/RoleUpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlogSite.Models
+{
+    public class RoleUpdateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Lütfen rol adı giriniz.")]
+        public string name { get; set; }
+    }
+}
---
+++ MyBlogSite/Models/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlogSite.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage="lütfen rol adı giriniz")]
+        public string name { get; set; }
+    }
+}
